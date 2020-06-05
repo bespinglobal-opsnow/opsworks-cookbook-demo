adf4f89edb4aee65f06f12c4888af1284583edc3
김영석Yeongseok Kim <yeongseok.kim@bespinglobal.com>
2020-06-05 14:02:21 +0900

    added sleeP

--- opsworks_cookbook_demo/logging/recipes/run_script.rb
+++ opsworks_cookbook_demo/logging/recipes/run_script.rb
@@ -3,8 +3,10 @@ script "Run a script" do
   interpreter "bash"
   code <<-EOH
     mkdir -m 777 /tmp/run-script-demo
-    touch /tmp/run-script-demo/helloworld.txt
+    DATE=date "+%Y-%m-%d_%H:%M:%S"
+    touch /tmp/run-script-demo/helloworld-$DATE.txt
     echo "Hello, World!" > /tmp/run-script-demo/helloworld.txt
     sudo aws s3 cp /tmp/run-script-demo/helloworld.txt s3://autospot-opsworks-bucket/helloworld.txt
+    sleep 180
   EOH
 end
