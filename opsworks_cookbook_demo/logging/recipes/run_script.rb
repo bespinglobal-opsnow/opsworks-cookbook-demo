ea56be578930bea797e80d51c775f20aec46424f
김영석Yeongseok Kim <yeongseok.kim@bespinglobal.com>
2020-06-05 14:27:04 +0900

    mofified helloword

--- opsworks_cookbook_demo/logging/recipes/run_script.rb
+++ opsworks_cookbook_demo/logging/recipes/run_script.rb
@@ -5,8 +5,8 @@ script "Run a script" do
     mkdir -m 777 /tmp/run-script-demo
     DATE=date "+%Y-%m-%d_%H:%M:%S"
     touch /tmp/run-script-demo/helloworld-$DATE.txt
-    echo "Hello, World!" > /tmp/run-script-demo/helloworld.txt
-    sudo aws s3 cp /tmp/run-script-demo/helloworld.txt s3://autospot-opsworks-bucket/helloworld.txt
-    sleep 180
+    echo "Hello, World!" > /tmp/run-script-demo/helloworld-$DATE.txt
+    sudo aws s3 cp /tmp/run-script-demo/helloworld-$DATE.txt s3://autospot-opsworks-bucket/helloworld-$DATE.txt
+    sleep 600
   EOH
 end
