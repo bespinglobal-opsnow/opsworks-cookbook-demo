1ac8de8a6c4fbdf1081d142449cbb89286ffd589
김영석Yeongseok Kim <yeongseok.kim@bespinglobal.com>
2020-06-05 10:21:03 +0900

    s3upload recipe added

---
+++ opsworks_cookbook_demo/s3upload/metadata.rb
@@ -0,0 +1,2 @@
+name "logging"
+version "0.1.0"
---
+++ opsworks_cookbook_demo/s3upload/recipes/run_script.rb
@@ -0,0 +1,11 @@
+Chef::Log.info("******Uploading logs to s3.******")
+
+script "Run a script" do
+  interpreter "bash"
+  code <<-EOH
+    mkdir -m 777 /tmp/run-script-demo
+    touch /tmp/run-script-demo/helloworld.txt
+    echo "Hello, World!" > /tmp/run-script-demo/helloworld.txt
+    sudo aws s3 cp /tmp/run-script-demo/helloworld.txt s3:///autospot-opsworks-bucket/helloworld.txt
+  EOH
+end
