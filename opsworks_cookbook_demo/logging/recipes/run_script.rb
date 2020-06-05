448f9fa4ec88228291e575b07d2445c0322b5d85
김영석Yeongseok Kim <yeongseok.kim@bespinglobal.com>
2020-06-05 16:25:51 +0900

    string concat

--- opsworks_cookbook_demo/logging/recipes/run_script.rb
+++ opsworks_cookbook_demo/logging/recipes/run_script.rb
@@ -4,9 +4,9 @@ script "Run a script" do
   code <<-EOH
     mkdir -m 777 /tmp/run-script-demo
     DATE=date "+%Y-%m-%d_%H:%M:%S"
-    touch /tmp/run-script-demo/helloworld-$DATE.txt
-    echo "Hello, World!" > /tmp/run-script-demo/helloworld-$DATE.txt
-    sudo aws s3 cp /tmp/run-script-demo/helloworld-$DATE.txt s3://autospot-opsworks-bucket/helloworld-$DATE.txt
+    touch /tmp/run-script-demo/helloworld-${DATE}.txt
+    echo "Hello, World!" > /tmp/run-script-demo/helloworld-${DATE}.txt
+    sudo aws s3 cp /tmp/run-script-demo/helloworld-${DATE}.txt s3://autospot-opsworks-bucket/helloworld-${DATE}.txt
     sleep 600
   EOH
 end
