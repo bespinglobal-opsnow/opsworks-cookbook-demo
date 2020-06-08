1c756384b66cf7f4b6d404b57c39f0a7cda926b2
김영석Yeongseok Kim <yeongseok.kim@bespinglobal.com>
2020-06-08 15:39:08 +0900

    modified logginng

--- opsworks_cookbook_demo/logging/recipes/run_script.rb
+++ opsworks_cookbook_demo/logging/recipes/run_script.rb
@@ -2,10 +2,11 @@ Chef::Log.info("******Uploading logs to s3.******")
 script "Run a script" do
   interpreter "bash"
   code <<-EOH
+    export TZ="Asia/Seoul"
+    date=$(date "+%Y-%m-%d_%H:%M:%S")
     mkdir -m 777 /tmp/run-script-demo
-    DATE=$(date "+%Y-%m-%d_%H:%M:%S")
-    touch /tmp/run-script-demo/helloworld-${DATE}.txt
-    echo "Hello, World!" > /tmp/run-script-demo/helloworld-${DATE}.txt
-    sudo aws s3 cp /tmp/run-script-demo/helloworld-${DATE}.txt s3://autospot-opsworks-bucket/helloworld-${DATE}.txt
+    instanceid=$( curl http://169.254.169.254/latest/meta-data/instance-id )
+    tar -czvf /tmp/run-script-demo/runs-${date}.tar.gz /var/chef/runs
+    sudo aws s3 cp /tmp/run-script-demo/runs-${date}.tar.gz s3://autospot-opsworks-bucket/$instanceid/runs-${date}.tar.gz
   EOH
 end
